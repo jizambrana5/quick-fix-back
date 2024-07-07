28e22898ae0e4f4841a902b8fc0c9dfc2136b4c2
José Ignacio Zambrana <jose.zambrana@mercadolibre.com>
2024-07-06 21:35:37 -0300

    Loggin library added

--- pkg/main.go
+++ pkg/main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/jizambrana5/quickfix-back/pkg/domain/order"
 	"github.com/jizambrana5/quickfix-back/pkg/domain/user"
+	"github.com/jizambrana5/quickfix-back/pkg/lib/logs"
 	"github.com/jizambrana5/quickfix-back/pkg/repository/database"
 	orderRepo "github.com/jizambrana5/quickfix-back/pkg/repository/database/order"
 	userRepo "github.com/jizambrana5/quickfix-back/pkg/repository/database/user"
@@ -14,6 +15,10 @@ import (
 )
 
 func main() {
+	// Initialize the logger based on the environment
+	logs.InitLogger("development")
+	defer logs.Logger.Sync() //nolint
+
 	// Ruta relativa al archivo JSON de ubicaciones en Mendoza
 	locations, err := utils.LoadLocations()
 	if err != nil {
